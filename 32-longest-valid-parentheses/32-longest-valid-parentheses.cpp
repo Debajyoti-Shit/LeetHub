88a6a28da2307b9f025ed7546c19300d36480615
Debajyoti <76768317+Debajyoti-Shit@users.noreply.github.com>
2022-05-24 08:30:19 +0530

    Time: 8 ms (37.09%), Space: 7.4 MB (20.23%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+    int MAX = 0;  // denotes length of maximum valid parentheses
+	vector<int> stk;
+	stk.push_back(-1); // bottom of stack will always hold index preceding to potential start of valid parentheses
+	for(int i = 0; i < size(s); i++)
+		if(s[i] == '(') stk.push_back(i);            
+		else{                
+			stk.pop_back();
+			if(stk.empty()) stk.push_back(i);
+			else MAX = max(MAX, i - stk.back());
+		}        
+	return MAX;
+    }
+};
