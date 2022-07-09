18a590eba07c70f49274ee6d28e249a6cded8854
Debajyoti <76768317+Debajyoti-Shit@users.noreply.github.com>
2022-07-09 09:51:33 +0530

    Added solution - LeetHub

--- Polynomial Addition - GFG/polynomial-addition.cpp
+++ Polynomial Addition - GFG/polynomial-addition.cpp
@@ -84,8 +84,6 @@ struct Node
 
 class Solution{
   public:
-    /* The below method print the required sum of polynomial
-    p1 and p2 as specified in output  */
     Node* addPolynomial(Node *p1, Node *p2){
         if(p1==NULL) return p2;
         if(p2==NULL) return p1;
@@ -94,10 +92,12 @@ class Solution{
             p1->coeff=p1->coeff + p2->coeff;
             p1->next=addPolynomial(p1->next,p2->next);
             return p1;
-        }else if(p1->pow > p2->pow){
+        }
+        else if(p1->pow > p2->pow){
             p1->next=addPolynomial(p1->next,p2);
             return p1;
-        }else{
+        }
+        else{
             p2->next=addPolynomial(p1,p2->next);
             return p2;
         }
