bdd2968ed57dfa08168a84e7bf163a3d45a16326
Debajyoti <76768317+Debajyoti-Shit@users.noreply.github.com>
2022-08-01 10:07:58 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (67.59%) - LeetHub

---
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
@@ -0,0 +1,16 @@
+int guess(int num);
+class Solution {
+public:
+    int guessNumber(int n) {
+        int low = 1, high = n;
+        int mid;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (guess(mid) == 0) return mid;
+            else if (guess(mid) ==-1) high = mid - 1;
+            else low = mid + 1;
+        }
+               
+        return mid;
+    }
+};
