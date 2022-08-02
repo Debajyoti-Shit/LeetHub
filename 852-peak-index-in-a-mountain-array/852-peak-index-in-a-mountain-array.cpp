b2edb9158854c360b99f6f4561a4c2b23a67c6f6
Debajyoti <76768317+Debajyoti-Shit@users.noreply.github.com>
2022-08-02 09:06:32 +0530

    Time: 199 ms (17.31%), Space: 59.7 MB (31.38%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
+        int lo=0,hi=arr.size()-1;
+        int mid= lo + (hi-lo)/2;
+        while(lo<hi){
+            
+            if(arr[mid] < arr[mid+1]) lo=mid+1;
+            else hi=mid;
+            mid= lo + (hi-lo)/2;
+        }
+        return lo;
+    }
+};
+/*
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
         for(int i=1;i<arr.size();i++){
             if(arr[i] < arr[i-1]) return i-1;
         }
         return -1;
     }
 };
+*/
