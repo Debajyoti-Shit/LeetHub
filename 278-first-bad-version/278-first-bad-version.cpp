2b707d3e7d484c0ebd6f1bc6552b533c47af0791
Debajyoti <76768317+Debajyoti-Shit@users.noreply.github.com>
2022-08-05 09:01:59 +0530

    Time: 0 ms (100.00%), Space: 6 MB (22.67%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -0,0 +1,15 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int lo=1,hi=n;
+        while(lo<hi){
+            int mid= lo + (hi-lo)/2;
+            if(!isBadVersion(mid)) lo=mid+1;
+            else hi=mid;
+        }
+        return lo;
+    }
+};
