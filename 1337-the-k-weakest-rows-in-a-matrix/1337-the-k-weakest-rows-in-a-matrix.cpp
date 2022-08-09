0f78befb9e6115341903f08132e5daed8dcc5ed4
Debajyoti <76768317+Debajyoti-Shit@users.noreply.github.com>
2022-08-09 16:05:08 +0530

    Time: 16 ms (72.04%), Space: 10.5 MB (41.45%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        int rows = mat.size(), cols = mat[0].size();
-        vector<int> ans(k,1);
-        multimap<int, int> m;
-        for(int i = 0; i < rows; i++) {
-            int ind = lower_bound(mat[i].rbegin(), mat[i].rend(), 1) - mat[i].rbegin();
-            cout << cols - ind << endl;
-            m.insert({cols - ind, i});
+        int n=mat[0].size();
+        for(int i=0; i<mat.size(); i++){
+            mat[i].push_back(i);
         }
-        int j = 0;
-        for(auto i : m){
-            if(j == k) break;
-            ans[j] = i.second;
-            j++;
-        } 
-        
+        sort(mat.begin(), mat.end());
+        vector<int> ans(k);
+        for(int i=0; i<k; i++){
+            ans[i]=mat[i][n];            
+        }
         return ans;
     }
 };
